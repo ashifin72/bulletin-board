228f1ea45a5f890974f5934b0e85815b943a0a38
Ashifin <ashfedor@gmail.com>
2021-01-20 13:46:19 +0200

    init Register Login

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/attributes-order': 'off',
+    'vue/no-confusing-v-for-v-if': 'off',
+    'vue/no-v-html': 'off',
+    'vue/order-in-components': 'off',
+    'vue/this-in-template': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/standard'
+  ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+        app
+      >
+        <v-list dense>
+          <v-list-item
+            v-for="link of links"
+            :key="link.title"
+            :to="link.url"
+          >
+            <v-list-item-action>
+              <v-icon>mdi-{{link.icon}}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{link.title}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar
+        app
+        color="indigo"
+        dark
+      >
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          class="hidden-md-and-up"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <router-link
+            :to="{name: 'home'}"
+            tag="span"
+            class="pointer">
+            Ad application
+          </router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn class="hidden-sm-and-down"
+               dark color="indigo"
+               v-for="link in links"
+               :key="link.title"
+               :to="link.url"
+        >
+          <v-icon left>mdi-{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-app-bar>
+      <v-main>
+
+          <router-view></router-view>
+
+      </v-main>
+      <v-footer
+        color="indigo"
+        app
+      >
+        <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data () {
+      return {
+        drawer: false,
+        links: [
+          {
+            title: 'Login',
+            icon: 'lock',
+            url: { name: 'login' }
+          },
+          {
+            title: 'Registration',
+            icon: 'face',
+            url: { name: 'registration' }
+          },
+          {
+            title: 'Orders',
+            icon: 'bookmark-check-outline',
+            url: { name: 'orders' }
+          },
+          {
+            title: 'New ad',
+            icon: 'pencil-plus',
+            url: { name: 'newAdd' }
+          },
+          {
+            title: 'My ads',
+            icon: 'clipboard-list-outline',
+            url: { name: 'list' }
+          }
+        ]
+      }
+    }
   }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
 </style>
